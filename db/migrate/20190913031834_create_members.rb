0db968891f851a53848ffedfac6516daf88422ee
corselia <takiya@toran.sakura.ne.jp>
2019-09-13 12:20:03 +0900

    Add a Member model structure

--- db/migrate/20190913031834_create_members.rb
+++ db/migrate/20190913031834_create_members.rb
@@ -1,6 +1,12 @@
 class CreateMembers < ActiveRecord::Migration[6.0]
   def change
     create_table :members do |t|
+      t.string :firstname
+      t.string :lastname
+      t.integer :age
+      t.integer :sex # enum
+      t.string :born_at_pref
+      t.boolean :is_japanese
 
       t.timestamps
     end
