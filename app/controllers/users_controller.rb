0842b7f9f25607392c35c97c028b7b8d5a9a09d2
corselia <takiya@toran.sakura.ne.jp>
2019-11-12 17:14:42 +0900

    S3へのアップロードの設定が完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def create
     user = User.new(user_params)
     user.save!
+
+    redirect_to action: :index
   end
 
   private
--- config/initializers/shirne.rb
+++ config/initializers/shirne.rb
@@ -1,9 +1,23 @@
 require 'shrine'
-require 'shrine/storage/file_system'
+require "shrine/storage/s3"
+
+s3_options = {
+  bucket:            ENV['S3_BUCKET_NAME'],
+  access_key_id:     ENV['S3_ACCESS_KEY_ID'],
+  secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+  region:            ENV['S3_REGION']
+}
 
 Shrine.storages = {
-  store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/store'),
-  cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache')
+  cache: Shrine::Storage::S3.new(prefix: 'test/shrine/cache', **s3_options),
+  store: Shrine::Storage::S3.new(prefix: 'test/shrine/store', **s3_options)
 }
+
+# require 'shrine/storage/file_system'
+
+# Shrine.storages = {
+#   store: Shrine::Storage::FileSystem.new('public', prefix: 'shrine/uploads/store'),
+#   cache: Shrine::Storage::FileSystem.new('public', prefix: 'shrine/uploads/cache')
+# }
 
 Shrine.plugin :activerecord
