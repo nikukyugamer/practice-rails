9e63530cef8fbe3346434bd88409d95cb58835dd
corselia <takiya@toran.sakura.ne.jp>
2019-11-26 11:52:57 +0900

    bundle exec rails db:migrate した

--- db/migrate/20191126024855_create_punditers.rb
+++ db/migrate/20191126024855_create_punditers.rb
@@ -1,6 +1,9 @@
 class CreatePunditers < ActiveRecord::Migration[6.0]
   def change
     create_table :punditers do |t|
+      t.string :name
+      t.string :place
+      t.integer :price
 
       t.timestamps
     end
