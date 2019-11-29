49c871e3c46068460dffe345f1ba440ee7671fff
corselia <takiya@toran.sakura.ne.jp>
2019-11-29 10:39:45 +0900

    Vue.js で axios を使う標準的なコードが完成

--- public/vuejs/axios/app.js
+++ public/vuejs/axios/app.js
@@ -1,15 +1,23 @@
 var vm = new Vue({
   el: '#app',
+  data: {
+    message: {}
+  },
   created: function() {
     self = this;
 
     axios
-        .get('./test.json')
+        // ローカルファイルを汎用的に読み込む方法
+        // https://medium.com/@negarjf/how-to-access-a-static-json-file-in-vue-cli-3-8943dc343f95
+        .get('http://localhost:8080/test.json')
+        // .get('./test.json')
+        // .get('https://qiita.com/api/v2/items?page=1&per_page=5')
         .then(function(res) {
           console.log(res);
+          self.message = res;
         })
         .catch(function(err) {
           console.log(err);
         });
-  }
+  },
 });
