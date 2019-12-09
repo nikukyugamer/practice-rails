8d0444169637f76ac52e57a06515163dfda3b95d
corselia <takiya@toran.sakura.ne.jp>
2019-12-09 11:09:51 +0900

    サンプルのtest.jsonファイルの絶対位置が変更になったので修正した

--- public/vuejs/ichiban_yasashii/app.js
+++ public/vuejs/ichiban_yasashii/app.js
@@ -9,7 +9,7 @@ var vm = new Vue({
     axios
         // ローカルファイルを汎用的に読み込む方法
         // https://medium.com/@negarjf/how-to-access-a-static-json-file-in-vue-cli-3-8943dc343f95
-        .get('http://localhost:8080/test.json')
+        .get('http://localhost:3000/vuejs/ichiban_yasashii/test.json')
         // .get('./test.json')
         // .get('https://qiita.com/api/v2/items?page=1&per_page=5')
         .then(function(res) {
