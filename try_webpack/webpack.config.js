7ec2356ac4884026fb50be3374f37571ca66ef50
corselia <takiya@toran.sakura.ne.jp>
2019-12-17 14:57:24 +0900

    create webpack.config.js

---
+++ try_webpack/webpack.config.js
@@ -0,0 +1,12 @@
+// output.path に指定するパスが OS によって異なることを防ぐために path モジュールを読み込む
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/app/js',
+  output: {
+    filename: 'bundle.js',
+    // 絶対パスを指定しないとエラーが出るので注意する
+    path: path.resolve(__dirname, 'public/js')
+  }
+};
