74e89fa5589725259685002d8e10026192c9e50f
corselia <takiya@toran.sakura.ne.jp>
2019-12-17 13:40:24 +0900

    プリントデバッグの行を削除した

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,3 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-console.log('Hello, Webpacker World!');
-console.log('Hello, Webpacker World!!');
