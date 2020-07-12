405dafe20d67b5b90644fd23baee2c82966373fa
Osamu Takiya <takiya@toran.sakura.ne.jp>
2020-07-12 19:01:49 +0900

    Add a flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     user.save!
 
-    redirect_to action: :index
+    redirect_to users_url, notice: '登録しました'
   end
 
   private
