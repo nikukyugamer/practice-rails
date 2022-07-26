d1abe6b7732c285a825af0756f748b015a6d437b
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-07-26 14:25:25 +0900

    feat: 🎸 rails g で生成されるファイルを調整した (#514)

--- config/application.rb
+++ config/application.rb
@@ -1,22 +1,30 @@
 require_relative "boot"
-
 require "rails/all"
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module ChallengingRails
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Asia/Tokyo'
+    config.active_record.default_timezone = :utc
+
+    I18n.config.available_locales = %i[en ja]
+    I18n.default_locale = :ja
+
+    config.generators do |g|
+      g.assets     false
+      g.helper     false
+
+      g.test_framework(
+        :rspec,
+        fixtures: true,
+        controller_specs: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+      )
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
